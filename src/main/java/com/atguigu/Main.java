0a7c1bcb61a9a41d7e5cc4a853a95430a810824b
Fhawk <651395250@qq.com>
2022-09-26 23:29:10 +0800

    master  commit3

--- src/main/java/com/atguigu/Main.java
+++ src/main/java/com/atguigu/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("Hello git4!");
         System.out.println("master git5!");
         System.out.println("hot fix git5!");
+        System.out.println("master git6!");
     }
 }
